ee70f09f04cd66e462e47d28ce0121f448cccddc
GartDev <garvdl@yahoo.com>
2018-04-12 13:58:42 -0400

    project start

---
+++ ssfsDisk.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+
+using namespace std;
+
+class ssfsDisk{
+    public:
+        int numBlocks;
+        int blockSize;
+        string 
+
+}
---
+++ ssfsFile.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include <string.h>
+
+using namespace std;
+
+class ssfsFile{
+    public:
+        string fileName;
+        int fileSize;
+        int * dBlocks [12];
+        int * 
+
+}
