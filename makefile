011c682fe6cd37c91895d1376ef9d98ff9f796c1
Leandro Ribeiro <lribeir1@cs.binghamton.edu>
2018-04-26 19:22:09 -0400

    edited makefile, multithreading working so far

--- makefile
+++ makefile
@@ -1,31 +1,27 @@
 # "make" to build and run both executables in order
 # "make disk" to build and run ssfs_mkdsk
-# "make sys" to build and run ssfs
+# "make ssfs" to build and run ssfs
 # "make clean" to delete all object and binary files
 
-run : disk sys
+all : ssfs ssfs_mkdsk
 
-
-
-disk : ssfs_mkdsk	
+disk : ssfs_mkdsk
 
 ssfs_mkdsk : mkdsk.o
 	g++ -Wall -g mkdsk.o -o ssfs_mkdsk
 
-mkdsk.o :   
+mkdsk.o : mkdsk.cpp
 	g++ -c mkdsk.cpp
-
-
-
-sys : ssfs
 
 ssfs : ssfs.o
 	g++ -Wall -g ssfs.o -pthread -o ssfs
 
-ssfs.o :
+ssfs.o : ssfs.cpp
 	g++ -c ssfs.cpp
-
-
 
 clean :
 	rm -f *.o ssfs_mkdsk ssfs DISK
+
+run: ssfs ssfs_mkdsk
+	./ssfs_mkdsk 1024 128 DISK
+	./ssfs DISK thread1.txt thread2.txt thread3.txt thread4.txt
--- ssfs.cpp
+++ ssfs.cpp
@@ -11,6 +11,11 @@ void *read_file(void *arg){
 	std::ifstream opfile;
 	char* file_name = (char*)arg;
 	opfile.open(file_name);
+	if(!opfile){
+		perror(file_name);
+		pthread_exit(NULL);
+	}
+	//print lines of the file, don't delete yet
 	for (std::string line; std::getline(opfile, line);) {
         std::cout << line << std::endl;
     }
@@ -19,16 +24,17 @@ void *read_file(void *arg){
 }
 
 int main(int argc, char **argv){
-	pthread_t *threads = new pthread_t[argc-2];
+	if(argc < 2){
+		std::cout << "Error: too few arguments." << std::endl;
+	}
+	pthread_t p;
 	int rc;
-	for(int i = 2; i < (argc-1); i++){
-		rc = pthread_create(&threads[i], NULL, read_file, (void*)argv[i]);
+	for(int i = 2; i < argc; i++){
+		rc = pthread_create(&p, NULL, read_file, (void*)argv[i]);
 		if(rc){
 			std::cout << "Unable to create thread." << std::endl;
 			exit(0);
 		}
 	}
-	delete [] threads;
-
 	pthread_exit(NULL);
 }
