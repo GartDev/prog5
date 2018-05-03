a628fef416ce1506cde320f936c0b66c8efd0e60
Kyle <kfranke1@binghamton.edu>
2018-05-03 18:30:18 -0400

    Working on char * stuff a bit more, got it to work

--- inode.h
+++ inode.h
@@ -4,19 +4,17 @@
 #include <sys/time.h>
 #include <string.h>
 
-using namespace std;
-
 class inode {
     public:
-	inode(string file_name, int file_size) {
+	inode(std::string file_name, int file_size) {
 		this->file_name = file_name;
 		this->file_size = file_size;	
 	}
 
-        string file_name;
+        std::string file_name;
         int file_size;
         int direct_blocks [12];
         int * indirect_block;
 	int ** double_indirect_block;
 
-}
+};
